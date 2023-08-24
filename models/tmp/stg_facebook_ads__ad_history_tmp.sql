3bfee3e8247d5081e01a54941061821d04ab5988
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-08-24 17:09:47 -0500

    MagicBot/add-union-schema updates

--- models/tmp/stg_facebook_ads__ad_history_tmp.sql
+++ models/tmp/stg_facebook_ads__ad_history_tmp.sql
@@ -1,4 +1,12 @@
-{{ config(enabled=var('ad_reporting__facebook_ads_enabled', True)) }}
-
-select * 
-from {{ var('ad_history') }}
+{{
+    fivetran_utils.union_data(
+        table_identifier='ad_history', 
+        database_variable='facebook_ads_database', 
+        schema_variable='facebook_ads_schema', 
+        default_database=target.database,
+        default_schema='facebook_ads',
+        default_variable='ad_history_source',
+        union_schema_variable='facebook_ads_union_schemas',
+        union_database_variable='facebook_ads_union_databases'
+    )
+}}
--- models/tmp/stg_facebook_ads__campaign_history_tmp.sql
+++ models/tmp/stg_facebook_ads__campaign_history_tmp.sql
@@ -1,4 +1,12 @@
-{{ config(enabled=var('ad_reporting__facebook_ads_enabled', True)) }}
-
-select * 
-from {{ var('campaign_history') }}
+{{
+    fivetran_utils.union_data(
+        table_identifier='campaign_history', 
+        database_variable='facebook_ads_database', 
+        schema_variable='facebook_ads_schema', 
+        default_database=target.database,
+        default_schema='facebook_ads',
+        default_variable='campaign_history_source',
+        union_schema_variable='facebook_ads_union_schemas',
+        union_database_variable='facebook_ads_union_databases'
+    )
+}}

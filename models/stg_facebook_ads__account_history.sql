c15ee30563e4efd27fea3d5f8a36b6cefc2dde65
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2024-05-22 14:18:14 -0700

    check redshift

--- models/stg_facebook_ads__account_history.sql
+++ models/stg_facebook_ads__account_history.sql
@@ -37,7 +37,10 @@ final as (
         created_time as created_at,
         currency,
         timezone_name,
-        row_number() over (partition by source_relation, id order by _fivetran_synced desc) = 1 as is_most_recent_record
+        case 
+            when id is null then row_number() over (partition by source_relation, id order by source_relation) = 1 
+            else row_number() over (partition by source_relation order by _fivetran_synced desc)
+            end = 1 as is_most_recent_record
     from fields
 
 )
